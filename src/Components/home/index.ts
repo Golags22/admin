9da0a808d8f9611ee05ee29c1a006f28f7ede07b
musicapp <153190928+Golags22@users.noreply.github.com>
2024-08-18 20:59:10 -0700

    khiu

---
+++ src/Components/home/index.ts
@@ -0,0 +1,26 @@
+import UpcomingEvents from "./upcoming-events";
+import  DealsChart  from "./deals-chart";
+import DashBoardTotalCountCard from "./total-count-card";
+import  UpcomingEventsSkeleton from '../skeleton/upcoming-events';
+import AccordionHeaderSkeleton from "../skeleton/accordion-header";
+import KanbanColumnSkeleton from "../skeleton/kanban";
+import LatestActivitiesSkeleton from "../skeleton/latest-activities";
+import ProjectCardSkeleton from "../skeleton/project-card";
+import LatestActivities from "./latest-activities";
+
+
+
+
+
+
+export {
+  UpcomingEvents,
+  DealsChart,
+DashBoardTotalCountCard,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+LatestActivities,
+};
---
+++ src/Components/layout/index.tsx
@@ -0,0 +1,19 @@
+import { ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd"
+import { Header } from "./headers";
+
+export const Layout = ({children}: React.PropsWithChildren) => {
+
+  return (
+
+    <ThemedLayoutV2
+    Header = { Header }
+Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
+    >
+      
+      {children}
+      
+      
+      </ThemedLayoutV2>
+  )
+}
+
